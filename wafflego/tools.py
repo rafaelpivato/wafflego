cb022874d4c238f9472bb35b995f161c0109b41b
Rafael Pivato <rafael@pivato.info>
2016-08-24 19:28:55 -0700

    Cards iterator with selector

--- wafflego/tools.py
+++ wafflego/tools.py
@@ -8,6 +8,8 @@ objects to help you accessing Waffle.io cards.
 
 """
 
+import requests
+
 from itertools import ifilter
 
 
@@ -55,8 +57,18 @@ class Spoon(object):
         self.bearer = bearer
         self.repo = repo
 
+    @property
+    def authorization(self):
+        return 'Bearer {0}'.format(self.bearer)
+
     def iter_cards(self, selector):
-        raise NotImplementedError()
+        url_format = 'https://api.waffle.io/{0}/cards'
+        url = url_format.format(self.repo)
+        headers = dict(authorization=self.authorization)
+        resp = requests.get(url, headers=headers)
+        for card in resp.json():
+            if selector(card):
+                yield card
 
 
 def main_done_selector(card):
